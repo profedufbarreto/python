a695e23efe9a6c64ff2f07eccf8201f217705db5
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-09 19:54:36 -0300

    usando input

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-for i in range(1, 5):
-    print("Teste", i)
+tab = int(input("Digite um valor para calcular: "))
+
+for i in range(1, 11):
+    resultado = tab * i
+    print(f"O resultado de {tab} * {i} = {resultado}")
