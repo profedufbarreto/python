a4cb338a97b0d29a0d7e990a1b68fe250ea14e5d
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-10 16:07:43 -0300

    funções

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-def soma(a, b):
-    resultado = a + b
-    return resultado
-
-def subt(a, b):
-    resultado = a - b 
-    return resultado
-
-def mult(a, b):
-    resultado = a * b
-    return resultado
-
-def div(a, b):
-    resultado = a / b
-    return resultado
-
-
-print(soma(5, 3))
-print(subt(8, 2))
-print(mult(8, 9))
-print(div(18, 9))
+def saudacao(nome, idade, cidade):
+    print(f"Olá {nome}!")
+    print(f"Você tem {idade} e mora em {cidade}!")
     
+    
+saudacao.nome = input("Digite seu nome: ")
+saudacao.idade = input("Digite sua idade: ")
+saudacao.cidade = input("Digite sua cidade: ")
+
+print(saudacao.nome, saudacao.idade, saudacao.cidade)
+print(saudacao.nome)
+print(saudacao.idade)
+print(saudacao.cidade)
+
+
