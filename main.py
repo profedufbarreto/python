be4ecc44b8600a5fde3afbeeea2f824edf333292
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-14 11:12:03 -0300

    classes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-nome = "Eduardo"
-texto = "Eu gosto de tomar café todos os dias"
-
-print(nome.upper())
-print(nome.lower())
-print(texto.replace("café", "mate"))
+class Carro:
+    def __init__(self, modelo, ano, cor):
+        self.modelo = modelo
+        self.ano = ano
+        self.cor = cor
+        
+    def cadastrar(self):
+        print(f"O {self.modelo} de {self.ano} e na cor {self.cor}foi cadastrado com sucesso!")
+        
+car1 = Carro("Corcel", 73, "Vermelha")
+car1.cadastrar()
