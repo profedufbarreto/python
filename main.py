f6674f1858e06c25632a7e27d13be12f3dfd27d1
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-14 21:27:20 -0300

    usando herança

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
-class Veiculo:
-    def __init__(self, modelo, ano, cor):
-        self.modelo = modelo 
-        self.ano = ano
-        self.cor = cor
-    
-    def cadastrar(self):
-        prinf(f"{self.modelo} do ano {self.ano} na cor {self.cor}.")
-        prinf(f"Foi cadastrado com sucesso!")
-        
-class Moto(Veiculo):
-    def __init__(self, modelo, ano, cor):
-        super().__init__(modelo, ano, cor)
-        self.rodas = rodas
+class Pessoa:
+    def __init__(self, nome, dataDeNascimento):
+        self.nome = nome
+        self.dataDeNascimento = dataDeNascimento
         
     def cadastrar(self):
-        print(f"{self.modelo} do ano {self.ano} na cor {self.cor} possuindo {self.rodas} rodas.")
-        print("Foi cadastrado com sucesso!")
+        print(f"{self.nome} nascimento em: {self.dataDeNascimento}")
+        print("Você foi cadastrado com sucesso!")
         
-tipo = input("Digite o tipo de veículo: ")
-
-modelo = input("Digite o modelo do veículo: ")
-ano = input("Digite o ano do veículo: ")
-cor = input("Digite a cor do veículo: ")
-
-if tipo.lower() == "moto":
-    rodas = input("Digite a quantidade de rodas: ")
-    vehichle = Moto(modelo, ano, cor, rodas)
-else:
-    vehichle = Veiculo(modelo, ano, cor)
-    
-vehichle.cadastrar()
+class Aluno(Pessoa):
+    def __init__(self, nome, dataDeNascimento, turma):
+        super().__init__(nome, dataDeNascimento)
+        self.turma = turma
         
+    def cadastrar(self):
+        print(f"{self.nome} nascido em: {self.dataDeNascimento} da turma {self.turma}.")
+        print("Você foi cadastrado com sucesso!")
+        
+p1 = Aluno("Eduardo", "11/07/1987", "321")
+p1.cadastrar()
