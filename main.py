ce167b3065e67234960f740afe899f25c0bce27f
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-16 14:59:30 -0300

    cadastro de livro e revista

--- main.py
+++ main.py
@@ -1,43 +1,90 @@
-class Pessoa:
-    def __init__(self, nome, dataDeNascimento):
-        self.nome = nome
-        self.dataDeNascimento = dataDeNascimento
+class Livro:
+    def __init__(self, titulo, autor, ano):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        self.disponivel = True
         
     def cadastrar(self):
-        print(f"{self.nome} nascimento em: {self.dataDeNascimento}")
-        print("Você foi cadastrado com sucesso!")
+        print(f"Livro: {self.titulo}, Autor: {self.autor}, ano: {self.ano}")
+        print("Cadastro realizado com sucesso")
         
-class Aluno(Pessoa):
-    def __init__(self, nome, dataDeNascimento, turma):
-        super().__init__(nome, dataDeNascimento)
-        self.turma = turma
-        
-    def cadastrar(self):
-        print(f"{self.nome} nascido em: {self.dataDeNascimento} da turma {self.turma}")
-        print("Você foi cadastrado com sucesso!")
-        
-class Professor(Pessoa):
-    def __init__(self, nome, dataDeNascimento, disciplina):
-        super().__init__(nome, dataDeNascimento)
-        self.disciplina = disciplina
-        
-    def cadastrar(self):
-        print(f"{self.nome} nascido em: {self.dataDeNascimento}, leciona {self.disciplina}")
-        print("Cadastro realizado com sucesso!")
-        
-print("Escolha a opção desejada para cadastro: ")
-opcao = input("1 - Professor | 2 - Aluno: ")
-
-nome = input("Digite o nome: ")
-dataDeNascimento = input("Digite a data de nascimento: ")
-
-if opcao == "1":
-    disciplina = input("Digite a disciplina: ")
-    pessoa = Professor(nome, dataDeNascimento, disciplina)
-elif opcao == "2":
-    turma = input("Digite a turma: ")
-    pessoa = Aluno(nome, dataDeNascimento, turma)
-else:
-    pessoa = Pessoa(nome, dataDeNascimento)
+    def emprestar(self):
+        if self.disponivel:
+            self.disponivel = False
+            print(f"O livro {self.titulo} foi emprestado com sucesso!")
+        else:
+            print(f"O livro {self.titulo} já está emprestado.")
+            
+    def devolver(self):
+        if not self.disponivel:
+            self.disponivel = True
+            print(f"O livro {self.titulo} foi devolvido com sucesso!")
+        else:
+            print(f"O livro {self.titulo} já estava disponível.")  
+            
+class Revista(Livro):
+    def __init__(self, titulo, autor, ano, edicao):
+        super().__init__(titulo, autor, ano)
+        self.edicao = edicao
     
-pessoa.cadastrar()
+    def cadastrar(self):
+        print(f"Revista: {self.titulo}, Autor: {self.autor}, ano: {self,ano}, edição: {self.edicao}")
+        print("Cadastro realizado com sucesso!!")
+        
+biblioteca = []
+
+while True:
+    print("\Escolha uma opção: ")
+    print("1 - Cadastrar Livro")
+    print("2 - Cadastrar Revista")
+    print("3 - Listar Itens")
+    print("4 - Emprestar Itens")
+    print("5 - Devolver Itens")
+    print("6 - Sair")
+    
+    opcao = input("Digite a opção: ")
+    
+    if opcao == "1":
+        titulo = input("Título do livro: ")
+        autor = input("Autor do livro: ")
+        ano = input("Ano do livro: ")
+        livro = Livro(titulo, autor, ano)
+        livro.cadastrar()
+        biblioteca.append(livro)
+        
+    elif opcao == "2":
+        titulo = input("Título da revista: ")
+        autor = input("Autor da revista: ")
+        ano = input("Ano da revista: ")
+        edicao = input("Edição da revista: ")
+        revista = Revista(titulo, autor, ano, edicao)
+        revista.cadastrar()
+        biblioteca.append(revista)
+    
+    elif opcao == "3":
+        print("\nItens cadastrados na biblioteca: ")
+        for i, item in enumerate(biblioteca, start=1):
+            status = "Disponível" if item.disponivel else "Emprestado"
+            print(f"{i} - {item.titulo} ({status})")
+            
+    elif opcao == "4":
+        indice = int(input("Digite o número do item para emprestar: ")) - 1
+        if 0 <= indice < len(biblioteca):
+            biblioteca[indice].emprestar()
+        else:
+            print("Item inválido.")
+    
+    elif opcao == "5":
+        indice = int(input("Digite o número do item para devolver: ")) -1
+        if 0 <= indice < len(biblioteca):
+            biblioteca[indice].devolver()
+        else:
+            print("Item inválido.")
+    
+    elif opcao == "6":
+        print("Encarrando o sistema...")
+        break
+    
+    else:
+        print("Opção inválida, tente novamente.")
